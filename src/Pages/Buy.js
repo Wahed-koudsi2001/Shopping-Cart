2603f9d7f7abcb921bb75bc08bdf4ec70a69ea53
Wahed Koudsi <82679451+Wahed-koudsi2001@users.noreply.github.com>
2025-12-29 15:51:09 +0300

    Delete src directory

--- src/Pages/Buy.js
+++
@@ -1,9 +0,0 @@
-import ShopBlock from "../Components/ShopBlock/ShopBlock"
-
-export default function Buy({ setCountCart }) {
-    return (
-        <div>
-            <ShopBlock setCountCart={setCountCart} />
-        </div>
-    )
-}
